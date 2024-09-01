249eb42f95c9395b1fe4958534feeb00cd6dfe7f
nicholas.silva <bnsilva5@misena.edu.co>
2024-08-31 22:48:36 -0500

    Configuraciones BackEnd

--- Back-app/src/app.js
+++ Back-app/src/app.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
-//import cors from 'cors';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.routes.js';
 
 const app = express()
 
-//app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+    origin: 'http://localhost:3000', // Reemplaza con el origen de tu frontend
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/", authRoutes);
 
 export default app
