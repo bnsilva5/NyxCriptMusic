fffab1517212c9050800f7c24383e98b2dc0a94f
nicholas.silva <bnsilva5@misena.edu.co>
2024-08-31 22:55:24 -0500

    Configuraciones FrontEnd

--- Front-app/src/App.js
+++ Front-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import CallbackHandler from "./components/CallbackHandler";
+import HomeLogin from "./components/HomeLogin";
+import HomeUser from "./components/HomeUser";
+import Playlists from "./components/Playlists";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/home-login" />} />
+                <Route path="/home-login" element={<HomeLogin />} />
+                <Route path="/home-user" element={<HomeUser />} />
+                <Route path="/auth/callback" element={<CallbackHandler />} />
+                <Route path="/user_playlists" element={<Playlists/>}/>
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
